e531b0246c3aee8625764561c033fdfc5358a130
yusufAbdulrasheed <yusufabdulrasheed200@gmail.com>
2024-08-07 08:56:32 +0100

    Added initial project files

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const PORT = process.env.SERVER_PORT || 5000
+const express = require('express')
+const app = express()
+
+
+// Database connection
+const mongoURI = process.env.MONGODB_CONNECTION
+
+mongoose.connect(mongoURI)
+.then(()=>{
+  console.log('Database Connected Successfully🥳🥳🥳🥳🥳🥳')
+  app.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT} 🔒🔒🔒🔒🔒🔒🔒`)
+  })
+})
+.catch((err) => {
+  console.log('There was an error connecting to the database, 😮😮😮😮😮😮', err)
+})
